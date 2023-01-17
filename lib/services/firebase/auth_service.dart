d4f42241223d90be6cba433f75107fd5d8685420
= <waseem@cognitivehealthintl.com>
2023-01-18 03:58:50 +0500

    Quiz History ui fixed signup ui fixed terms and conditions and privacy policy text github link creation left hint section left quiz history detail screen left

--- lib/services/firebase/auth_service.dart
+++ lib/services/firebase/auth_service.dart
@@ -5,7 +5,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:get/get.dart';
@@ -116,21 +115,6 @@ Future<UserCredential> signInWithGoogle() async {
 
   return await FirebaseAuth.instance.signInWithCredential(credential);
 }
-
- signInWithFacebook() async {
-  // Trigger the sign-in flow
-  final LoginResult loginResult = await FacebookAuth.instance.login();
-
-  if(loginResult.accessToken != null){
-    final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(loginResult.accessToken!.token);
-    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
-  }
-
-  // Create a credential from the access token
-
-  // Once signed in, return the UserCredential
-}
-
 
 performSignOut() {
   userController.userModel.value = UserModel();
